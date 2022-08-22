5debb15cee4e38a2a20af7dc42098a2a51a14a43
jeronimo aguirre <jeronimo.aguirre@upb.edu.co>
2022-08-21 22:48:49 -0500

    Version 6

---
+++ lib/vistas/comidas.dart
@@ -0,0 +1,43 @@
+class Pollo {
+  String nombre = "Pollo a la plancha";
+  int precio = 15000;
+}
+
+class Solomo {
+  String nombre = "Solomo a la Plancha";
+  int precio = 17000;
+}
+
+class Tomahawk {
+  String nombre = "Tomahawk";
+  int precio = 35000;
+}
+
+class Chicharron {
+  String nombre = "Chicharron";
+  int precio = 12000;
+}
+
+class Cerdo {
+  String nombre = "Filete de Cerdo";
+  int precio = 15000;
+}
+
+class Robalo {
+  String nombre = "Robalo Apanado";
+  int precio = 17000;
+}
+
+class Tilapia {
+  String nombre = "Tilapia";
+  int precio = 17000;
+}
+
+class Producto {
+  String nombre = "";
+  int precio = 0;
+  Producto(String nombre, int precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+}
